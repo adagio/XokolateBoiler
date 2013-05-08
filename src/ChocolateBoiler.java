7123a51e2da9a4d8ed1dc63405f3b73caca4cd31
Franco Cedillo <franco.cedillo@gmail.com>
2013-05-07 19:03:43 -0500

    totally mess with warnings

--- src/ChocolateBoiler.java
+++ src/ChocolateBoiler.java
@@ -1,12 +1,28 @@
 
-public class ChocolateBoiler {
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
+public class ChocolateBoiler {
+	
 	private boolean empty;
 	private boolean boiled;
 	
-	public ChocolateBoiler() {
+	private static ChocolateBoiler uniqueInstance;
+	
+	private final static Logger LOGGER = Logger.getLogger(ChocolateBoiler.class.getName());
+	
+	private ChocolateBoiler() {
 		empty = true;
 		boiled = false;
+		
+	    LOGGER.setLevel(Level.WARNING);
+	}
+	
+	public static ChocolateBoiler getInstance() {
+		if (uniqueInstance == null) {
+			uniqueInstance = new ChocolateBoiler();
+		}
+		return uniqueInstance;
 	}
 	
 	public void fill() {
@@ -15,6 +31,9 @@ public class ChocolateBoiler {
 			boiled = false;
 			// fill the boiler with a milk/chocolate mixture 
 			System.out.println("Chocolate boiler: filling the boiler");
+		} else {
+			System.out.println("Chocolate boiler: couldn't fill. was not empty");
+			LOGGER.warning("Chocolate boiler: couldn't fill. was not empty");
 		}
 	}
 	
@@ -23,6 +42,12 @@ public class ChocolateBoiler {
 			// drain the boiled milk and chocolate
 			System.out.println("Chocolate boiler: draining the boiler");
 			empty = true;
+		} else if (isEmpty()) {
+			System.out.println("Chocolate boiler: couldn't drain. was empty");
+			LOGGER.warning("Chocolate boiler: couldn't drain. was empty");
+		} else {
+			System.out.println("Chocolate boiler: couldn't drain. was not boiled");
+			LOGGER.warning("Chocolate boiler: couldn't drain. was empty");
 		}
 	}
 	
@@ -31,6 +56,12 @@ public class ChocolateBoiler {
 			// bring the contents to a boil
 			System.out.println("Chocolate boiler: boiling");
 			boiled = true;
+		} else if (isEmpty()) {
+			System.out.println("Chocolate boiler: couldn't boil. was empty");
+			LOGGER.warning("Chocolate boiler: couldn't boil. was empty");
+		} else {
+			System.out.println("Chocolate boiler: couldn't boil. was already boiled");
+			LOGGER.warning("Chocolate boiler: couldn't boil. was already boiled");
 		}
 	}
 	
--- src/WantMilk.java
+++ src/WantMilk.java
@@ -7,8 +7,8 @@ public class WantMilk {
 	public static void main(String[] args) {
 		
 		
-		ChocolateBoiler myChocolateBoiler = new ChocolateBoiler();
-		ChocolateBoiler myChocolateBoiler2 = new ChocolateBoiler();
+		ChocolateBoiler myChocolateBoiler = ChocolateBoiler.getInstance();
+		ChocolateBoiler myChocolateBoiler2 = ChocolateBoiler.getInstance();
 		
 		
 		
@@ -17,66 +17,55 @@ public class WantMilk {
 		myChocolateBoiler.printStatus();
 		System.out.println("FIRST INSTANCE ENDS");
 		
-		System.out.println("SECOND INSTANCE STARTS");
-		System.out.println("Initial Status:");
+		
+		System.out.println("                      SECOND INSTANCE STARTS");
+		System.out.println("                      Initial Status:");
 		myChocolateBoiler2.printStatus();
-		System.out.println("We are going to fill.");
+		System.out.println("                      We are going to fill.");
 		myChocolateBoiler2.fill();
-		System.out.println("SECOND INSTANCE ENDS");
+		System.out.println("                      After filling Status:");
+		myChocolateBoiler2.printStatus();
+		System.out.println("                      SECOND INSTANCE ENDS");
 		
 		
 		System.out.println("FIRST INSTANCE STARTS");
 		System.out.println("We are going to fill.");
 		myChocolateBoiler.fill();
 		System.out.println("After filling Status:");
-		System.out.println("FIRST INSTANCE ENDS");
-		
-		System.out.println("SECOND INSTANCE STARTS");
-		System.out.println("After filling Status:");
-		myChocolateBoiler2.printStatus();
-		System.out.println("We are going to boil.");
-		System.out.println("SECOND INSTANCE ENDS");
-		
-		
-		System.out.println("SECOND INSTANCE STARTS");
-		myChocolateBoiler2.boil();
-		System.out.println("After boiling Status:");
-		myChocolateBoiler2.printStatus();
-		System.out.println("SECOND INSTANCE ENDS");
-		
-		System.out.println("FIRST INSTANCE STARTS");
 		myChocolateBoiler.printStatus();
 		System.out.println("FIRST INSTANCE ENDS");
+		
+		
+		System.out.println("                      SECOND INSTANCE STARTS");
+		System.out.println("                      We are going to boil.");
+		myChocolateBoiler2.boil();
+		System.out.println("                      After boiling Status:");
+		myChocolateBoiler2.printStatus();
+		System.out.println("                      SECOND INSTANCE ENDS");
 		
 		
 		System.out.println("FIRST INSTANCE STARTS");
 		System.out.println("We are going to boil.");
 		myChocolateBoiler.boil();
 		System.out.println("After boiling Status:");
+		myChocolateBoiler.printStatus();
 		System.out.println("FIRST INSTANCE ENDS");
 		
-		System.out.println("SECOND INSTANCE STARTS");
-		System.out.println("We are going to drain.");
+		System.out.println("                       SECOND INSTANCE STARTS");
+		System.out.println("                       We are going to drain.");
 		myChocolateBoiler2.drain();
-		System.out.println("After boiling Status:");
-		System.out.println("SECOND INSTANCE ENDS");
+		System.out.println("                       After boiling Status:");
+		myChocolateBoiler2.printStatus();
+		System.out.println("                       SECOND INSTANCE ENDS");
 		
 		
 		System.out.println("FIRST INSTANCE STARTS");
-		myChocolateBoiler.printStatus();
-		System.out.println("FIRST INSTANCE ENDS");
-		
-		System.out.println("FIRST INSTANCE STARTS");
-		myChocolateBoiler.printStatus();
 		System.out.println("We are going to drain.");
 		myChocolateBoiler.drain();
 		System.out.println("After boiling Status:");
+		myChocolateBoiler.printStatus();
 		System.out.println("FIRST INSTANCE ENDS");
 
-		
-		System.out.println("SECOND INSTANCE STARTS");
-		myChocolateBoiler2.printStatus();
-		System.out.println("SECOND INSTANCE ENDS");
 		
 		
 		
